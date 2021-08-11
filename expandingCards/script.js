303cf0c33941672fd4456efa8fa16fc460873d1e
ibo <41275178+1B05H1N@users.noreply.github.com>
2021-08-11 10:17:19 -0700

    section 11

--- expandingCards/script.js
+++ expandingCards/script.js
@@ -1,14 +1,20 @@
-const panels = document.querySelectorAll('.panel')
+const insert = document.getElementById('insert')
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = `
+  <div class="key">
+  ${event.key === ' ' ? 'Space' : event.key} 
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
+</div>
+  `
 })
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
